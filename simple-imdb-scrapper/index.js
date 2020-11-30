b0e5d13483a08ab190740ef8e019087bca78698c
Matthew Doles <matteddied@gmail.com>
2020-11-30 15:44:53 -0600

    3.1 - How to spoof / add custom request headers

--- simple-imdb-scrapper/index.js
+++ simple-imdb-scrapper/index.js
@@ -4,7 +4,22 @@ const cheerio = require('cheerio');
 const URL = 'https://www.imdb.com/title/tt0102926/';
 
 (async () => {
-  const response = await request(URL);
+  const response = await request({
+    uri: URL,
+    headers: {
+      Accept:
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Cache-Control': 'max-age=0',
+      Connection: 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      Host: 'www.imdb.com',
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+    },
+    gzip: true,
+  });
   let $ = cheerio.load(response);
 
   let title = $('div[class="title_wrapper"] > h1').text();
