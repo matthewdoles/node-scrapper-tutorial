44b9afbe549fd5d1f8cdaafa6aa2a667a51e3bc7
Matthew Doles <matteddied@gmail.com>
2020-12-06 17:48:52 -0500

    5.3 - Parsing latests posts of an ig user

--- instagram-scrapper/index.js
+++ instagram-scrapper/index.js
@@ -20,12 +20,43 @@ const BSAE_URL = `https://instagram.com/${USERNAME}`;
     },
   } = JSON.parse(scriptRegex[1]);
 
+  const {
+    entry_data: {
+      ProfilePage: {
+        [0]: {
+          graphql: {
+            user: {
+              edge_owner_to_timeline_media: { edges },
+            },
+          },
+        },
+      },
+    },
+  } = JSON.parse(scriptRegex[1]);
+
+  const posts = [];
+  for (let edge of edges) {
+    const { node } = edge;
+    posts.push({
+      id: node.id,
+      shortcode: node.shortcode,
+      timestamp: node.taken_at_timestamp,
+      likes: node.edge_liked_by.count,
+      comments: node.edge_media_to_comment.count,
+      videoViews: node.video_view_count,
+      caption: node.edge_media_to_caption.edges[0].node.text,
+      imageUrl: node.display_url,
+    });
+  }
+
   const instagramData = {
     followers: user.edge_followed_by.count,
     following: user.edge_follow.count,
     uploads: user.edge_owner_to_timeline_media.count,
     fullName: user.full_name,
     pictureURL: user.profile_pic_url_hd,
+    posts,
   };
   console.log(instagramData);
+  debugger;
 })();
