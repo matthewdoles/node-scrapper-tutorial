55c673e74a85184050d67ac620474bfbb06edb8d
Matthew Doles <matteddied@gmail.com>
2020-12-07 19:56:28 -0500

    6.2 - Writing automated Google search

--- puppeteer-example/index.js
+++ puppeteer-example/index.js
@@ -4,7 +4,10 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://google.com');
-  await page.screenshot({ path: './puppeteer-example/example.png' });
+  await page.type('input[title="Search"]', 'Udemy Tutorials', { delay: 100 });
+  await page.keyboard.press('Enter');
+  await page.waitForNavigation();
+  await page.screenshot({ path: './puppeteer-example/udemy.png' });
 
   await browser.close();
 })();
