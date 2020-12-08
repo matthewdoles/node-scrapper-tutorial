485b3055e354a6a30bfbf96a2e14724b99d5402a
Matthew Doles <matteddied@gmail.com>
2020-12-08 17:50:30 -0500

    6.4 - Using Puppeteer to login to Instagram

--- puppeteer-example/example.js
+++ puppeteer-example/example.js
@@ -3,12 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://instagram.com');
-  await page.waitForSelector('input[name="username"]');
-  await page.type('input[name="username"]', 'matteddied');
-  await page.type('input[name="password"]', 'MustBeNice1');
+  await page.goto('https://google.com');
+  await page.type('input[title="Search"]', 'Udemy Tutorials', { delay: 100 });
   await page.keyboard.press('Enter');
   await page.waitForNavigation();
+  await page.screenshot({ path: './puppeteer-example/udemy.png' });
 
   await browser.close();
 })();
