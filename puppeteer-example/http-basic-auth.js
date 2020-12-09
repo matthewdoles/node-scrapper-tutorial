5a091a8b14cfe55368cc8819682664f39732a0d1
Matthew Doles <matteddied@gmail.com>
2020-12-08 19:39:39 -0500

    6.6 - HTTP Basic Auth

---
+++ puppeteer-example/http-basic-auth.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.authenticate({ username: 'admin', password: '123456' });
+  await page.goto('https://httpbin.org/basic-auth/admin/123456');
+
+  //await browser.close();
+})();
