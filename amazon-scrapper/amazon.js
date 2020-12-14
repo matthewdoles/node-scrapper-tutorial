65c02b1da3b0c335038bb0b69554787c2746073c
Matthew Doles <matteddied@gmail.com>
2020-12-14 18:17:16 -0500

    8.2 - Parsing Product Details

--- amazon-scrapper/amazon.js
+++ amazon-scrapper/amazon.js
@@ -11,6 +11,29 @@ const amazon = {
     page = await browser.newPage();
     await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
   },
+  getProductInfo: async (link) => {
+    await page.goto(link, { waitUntil: 'networkidle2' });
+    const details = await page.evaluate(() => {
+      const title = document.querySelector('#productTitle').innerText;
+      const manufacturer = document.querySelector('#bylibeInfo').innerText;
+      const currentPrice = document.querySelector('#priceblock_dealprice')
+        .innerText;
+      const rating = document
+        .querySelector('#acrPopover')
+        .getAttribute('title');
+      const totalRatings = document.querySelector('#acrCustomerReviewText')
+        .innerText;
+
+      return {
+        title,
+        manufacturer,
+        currentPrice,
+        rating,
+        totalRatings,
+      };
+    });
+    return details;
+  },
   end: async () => {
     await browser.close();
   },
--- amazon-scrapper/index.js
+++ amazon-scrapper/index.js
@@ -3,5 +3,12 @@ const amazon = require('./amazon');
 
 (async () => {
   await amazon.initialize();
+  const mac = await amazon.getProductInfo(
+    'https://www.amazon.com/Apple-2-7GHz-ME086LL-Desktop-Refurbished/dp/B00M4LWXI0'
+  );
+  const backpack = await amazon.getProductInfo(
+    'https://www.amazon.com/Thule-Crossover-32L-Backpack-Black/dp/B004XANKVO'
+  );
+  console.log(mac, backpack);
   await amazon.end();
 })();
