d45f1cda0569ee7dbe2ba6fc0b9255b267b347f7
Matthew Doles <matteddied@gmail.com>
2020-12-15 18:57:17 -0500

    9.2 - Dev Tools, Viewport, and Scrolling on Nightmare.js

--- nightmarejs-example/example.js
+++ nightmarejs-example/example.js
@@ -1,8 +1,14 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare({ show: true });
+const nightmare = Nightmare({
+  show: true,
+  openDevTools: {
+    mode: 'detach',
+  },
+});
 
 (async () => {
   try {
+    await nightmare.viewport(1200, 600);
     await nightmare.goto('https://duckduckgo.com');
     await nightmare.type('#search_form_input_homepage', 'github nightmare');
     await nightmare.click('#search_button_homepage');
@@ -10,6 +16,8 @@ const nightmare = Nightmare({ show: true });
     let link = await nightmare.evaluate(
       () => document.querySelector('#r1-0 a.result__a').href
     );
+    const height = await nightmare.evaluate(() => document.body.scrollHeight);
+    await nightmare.scrollTo(height, 0);
     await nightmare.end();
     console.log(link);
   } catch (error) {
