07b284d70d17d8182e72a02076c7f4e4cbbc0940
Matthew Doles <matteddied@gmail.com>
2020-12-14 19:07:57 -0500

    8.3 - Multi querySelector & how to console log

--- amazon-scrapper/amazon.js
+++ amazon-scrapper/amazon.js
@@ -9,20 +9,28 @@ const amazon = {
   initialize: async () => {
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
+    page.on('console', (message) => console.log(`Message: ${message.text()}`));
     await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
   },
   getProductInfo: async (link) => {
     await page.goto(link, { waitUntil: 'networkidle2' });
     const details = await page.evaluate(() => {
       const title = document.querySelector('#productTitle').innerText;
-      const manufacturer = document.querySelector('#bylibeInfo').innerText;
-      const currentPrice = document.querySelector('#priceblock_dealprice')
-        .innerText;
+      const manufacturer = document.querySelector('#bylineInfo').innerText;
+      const currentPrice = document.querySelector(
+        '#priceblock_ourprice,#priceblock_dealprice'
+      ).innerText;
       const rating = document
         .querySelector('#acrPopover')
         .getAttribute('title');
       const totalRatings = document.querySelector('#acrCustomerReviewText')
         .innerText;
+
+      console.log(title);
+      console.log(manufacturer);
+      console.log(currentPrice);
+      console.log(rating);
+      console.log(totalRatings);
 
       return {
         title,
