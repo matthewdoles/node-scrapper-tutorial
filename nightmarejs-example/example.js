8483af987a9cdbfecefd1ba82f32bc551d0b65fa
Matthew Doles <matteddied@gmail.com>
2020-12-14 22:05:48 -0500

    9.1 - Installin and introduction to Nightmare.js

---
+++ nightmarejs-example/example.js
@@ -0,0 +1,18 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+
+(async () => {
+  try {
+    await nightmare.goto('https://duckduckgo.com');
+    await nightmare.type('#search_form_input_homepage', 'github nightmare');
+    await nightmare.click('#search_button_homepage');
+    await nightmare.wait('#r1-0 a.result__a');
+    let link = await nightmare.evaluate(
+      () => document.querySelector('#r1-0 a.result__a').href
+    );
+    await nightmare.end();
+    console.log(link);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
